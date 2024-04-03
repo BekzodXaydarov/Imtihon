248fe5a306502a9833c304757100bbc4f06529f2
aziz0124 <fotihabdulaziz52@gmail.com>
2024-04-03 15:21:30 +0500

    second

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 import React from 'react'
 import './App.css'
+import Section from './section/section'
 
 export default function App() {
   return (
     <div className='container'>
-      <div className='container_textdiv'>
-        <p className='textdiv_p1'>
-          How to download TikTok videos online from the address bar?
-        </p>
-        <hr />
-        <p className='textdiv_p2'>Our TikTok Downloader works on any device (including, but not limited to Android, iOS, Windows, Linux, and MacOS). You do not need to install any software on your mobile phone or PC, all that you need is only a video link.</p>
-        <div className='textdiv_card'>
-          <div className='card_textp'>
-            <div className='textp_radius'></div><p className='textp_p'>Open Tik Tok app on your phone/or Web on your browser.</p>
-          </div>
-          <div className='card_textp'>
-            <div className='textp_radius'></div><p className='textp_p'>Choose whatever video you want to download.</p>
-          </div>
-          <div className='card_textp'>
-            <div className='textp_radius'></div><p className='textp_p'>Click to the Share button at the right bottom.</p>
-          </div>
-          <div className='card_textp'>
-            <div className='textp_radius'></div><p className='textp_p'>Click the Copy Link button.</p>
-          </div>
-        </div>
-      </div>
 
+<Section />
     </div>
   )
 }
--- src/section/section.js
+++ src/section/section.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import './section.css'
+import Img from '../assets/images/Ellipse 9.png'
 
-export default function section() {
+export default function Section() {
   return (
     <div className='container'>
       <div className='container_textdiv'>
@@ -22,6 +23,27 @@ export default function section() {
           </div>
           <div className='card_textp'>
             <div className='textp_radius'></div><p className='textp_p'>Click the Copy Link button.</p>
+          </div>
+        </div>
+      </div>
+      <div className='container_textdiv'>
+        <p className='textdiv_p1'>
+          How to download TikTok videos online from the address bar?
+        </p>
+        <hr />
+        <p className='textdiv_p2'>Our TikTok Downloader works on any device (including, but not limited to Android, iOS, Windows, Linux, and MacOS). You do not need to install any software on your mobile phone or PC, all that you need is only a video link.</p>
+        <div className='textdiv_card'>
+          <div className='card_textp'>
+           <img src={Img} alt="" /><p className='textp_p'>Open Tik Tok app on your phone/or Web on your browser.</p>
+          </div>
+          <div className='card_textp'>
+          <img src={Img} alt="" /><p className='textp_p'>Choose whatever video you want to download.</p>
+          </div>
+          <div className='card_textp'>
+          <img src={Img} alt="" /><p className='textp_p'>Click to the Share button at the right bottom.</p>
+          </div>
+          <div className='card_textp'>
+          <img src={Img} alt="" /><p className='textp_p'>Click the Copy Link button.</p>
           </div>
         </div>
       </div>
